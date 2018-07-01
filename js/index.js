53b321fcb5d60d0d509a93407596a1ac48c59f74
emmanuel ajaero <emmyprinso@gmail.com>
2018-07-01 23:48:31 +0100

    solved service work issue and update currencies offline

--- js/index.js
+++ js/index.js
@@ -1,15 +1,90 @@
+  //create idb here
+      console.log('testing idb before creating a db : ', idb);
+      let dbPromise = idb.open('ALC-currenct-db', 2, upgradeDb => {
+        console.log('start creating db');
+        switch(upgradeDb.oldVersion) {
+          case 0:
+            var keyValStore = upgradeDb.createObjectStore('convertion-rate');
+            keyValStore.put("{an objectwith all the pairs rate}", "single currency");
+          case 1:
+            upgradeDb.createObjectStore('currencies');
+            //upgradeDb.createObjectStore('currencies', { keyPath: 'currencyName' });
+        }
+       });
+
+    //for test purpose read that first entry
+    dbPromise.then(db => {
+      let tx = db.transaction('convertion-rate');
+      let keyValStore = tx.objectStore('convertion-rate');
+      return keyValStore.get('single currency');
+    }).then(val => {
+      console.log('"single currency" :', val);
+  });
+
 
 let serviceReg = () =>{
+
   if(!navigator.serviceWorker) return;
-  navigator.serviceWorker.register('js/sw.js').then(reg =>{
-    console.log("successfully register service worker", reg.waiting, reg.installing, reg.active);
-  }).catch(err => {
+  navigator.serviceWorker
+  .register('./js/sw.js')
+  .then(reg =>{
+
+    reg.addEventListener('statechange',() =>{
+      if(reg.waiting){
+        currenciesDisplay();
+      }
+    });
+
+    reg.addEventListener('updatefound', () => {
+      fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonval => {
+          console.log('---------------------currencies-------------');
+          dbPromise.then(db => {
+            let tx = db.transaction('currencies', 'readwrite');
+            let currenciesStore = tx.objectStore('currencies');
+
+         // for(const value in jsonval.results){
+            currenciesStore.put(jsonval.results, 'all');
+
+
+              //console.log(jsonval.results[value]);
+         // }
+
+          return tx.complete;
+        }).then(() => {
+         console.log('currencies updated');
+         currenciesDisplay();
+        });
+
+        
+      })
+    .catch(err => {
+      console.log('---------------------currencies fetch error-------------', err);
+    });
+
+      console.log("from the deep [installing] [waiting] [active]", reg.installing, reg.waiting, reg.active);
+
+    });
+
+
+    console.log("successfully register service worker", reg);
+    currenciesDisplay();
+
+
+  })
+  .catch(err => {
     console.log("failed for register service worker", err);
-  });
+  })
+
 }
 
 
 console.log("servive worker controller state", navigator.serviceWorker.controller);
+serviceReg();
+
 
 
 
@@ -20,4 +95,42 @@ function changeText(id) {
     //id.innerHTML = "Ooops!";
     console.log(id);
     we = id;
+}
+
+
+currenciesDisplay = () => {
+  console.log('################################################################################################');
+  dbPromise.then(db => {
+    let tx = db.transaction('currencies');
+    var keyValStore = tx.objectStore('currencies');
+    return keyValStore.get('all');
+   }).then(vals => {
+    console.log(vals);
+    let currenciesArray = [];
+    for(const val in vals){
+      currenciesArray.push(vals[val]);
+      //currenciesArray.push(vals[val].currencyName);
+    }
+    const sortCurrencies = Array.from(currenciesArray).sort((previous, next) => {
+      // Sort the currencies in Alphebetical order
+      if (previous.currencyName < next.currencyName) {
+          return -1;
+      }
+      else if (previous.currencyName > next.currencyName) {
+          return 1;
+      }
+      return 0;
+    });
+
+        // Populate currencies
+      return sortCurrencies.map(currency => {
+          const option = document.createElement('option');
+          option.value = currency.id;
+          option.textContent = `${currency.currencyName} (${currency.id})`;
+          document.querySelector('#fromCurrency').appendChild(option);
+          document.querySelector('#toCurrency').appendChild(option.cloneNode(true));
+      });
+
+   });
+
 }
--- js/sw.js
+++ js/sw.js
@@ -1,28 +1,57 @@
-
-
- let staticCacheName = 'alc-static-v1';
+let staticCacheName = 'alc-static-v3';
+ const toCache = [
+  '../',
+  '../index.html',
+  '../js/index.js',
+  '../js/idb.js',
+  '../css/style.css',
+  '../css/font-awesome.min.css',
+  'https://fonts.googleapis.com/css?family=Alegreya+Sans',
+  'https://fonts.googleapis.com/css?family=Tangerine',
+  'https://fonts.googleapis.com/css?family=Rancho&effect=shadow-multiple',
+  'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+  'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+];
 
    self.addEventListener('install', event => {
+     console.log('Install Event Running',location.pathname);
     event.waitUntil(
       caches.open(staticCacheName).then(cache => {
-        return cache.addAll([
-          '../',
-          '../index.html',
-          '../js/index.js',
-          '../js/idb.js',
-          '../css/style.css',
-          '../css/font-awesome.min.css',
-          'https://fonts.googleapis.com/css?family=Alegreya+Sans',
-          'https://fonts.googleapis.com/css?family=Tangerine',
-          'https://fonts.googleapis.com/css?family=Rancho&effect=shadow-multiple',
-          'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-          'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-        ]);
+        return cache.addAll(toCache);
       })
     );
+
+
+
+    
   });   
 
 
+  self.addEventListener('activate', event => {
+    console.log('[ServiceWorker] Activated');
+
+    event.waitUntil(
+
+    	// Get all the cache keys (cacheName)
+		caches.keys().then(cacheNames => {
+			return Promise.all(cacheNames.map(thisCacheName => {
+
+				// If a cached item is saved under a previous cacheName
+				if (thisCacheName !== staticCacheName && thisCacheName.startsWith('alc-static')) {
+
+					// Delete that cached file
+					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+					return caches.delete(thisCacheName);
+				}
+			}));
+		})
+	); // end event.waitUntil
+
+});
+
+
+
+/*
   self.addEventListener('fetch', function(event) {
     // TODO: respond with an entry from the cache if there is one.
     // If there isn't, fetch from the network.
@@ -34,4 +63,4 @@
    })
    );
    });
-   
+   */
